f6a6eb73b49b837068d3830ef7e8092b4f7a95cf
CanTrinh <cannhai1995@gmail.com>
2026-02-15 21:22:33 +0700

    doi pass

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: "myBackendNestjs",
       script: "dist/src/main.js",
       env: {
-        DATABASE_URL: "mysql://admin:Cantrinh1995@mydatabase.c5iggco2eked.ap-southeast-2.rds.amazonaws.com:3306/msql",
+        DATABASE_URL: "mysql://admin:Saostar1995@mydatabase.c5iggco2eked.ap-southeast-2.rds.amazonaws.com:3306/msql",
         JWT_SECRET: "hskhsierihn3759kfjjruiurkfur",
         JWT_EXPIRES_IN: "1h"
       }
