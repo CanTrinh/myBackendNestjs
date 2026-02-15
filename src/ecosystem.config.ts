402d0071eb58c2501d7e2c0783e30974a97139b5
CanTrinh <cannhai1995@gmail.com>
2026-02-15 21:03:35 +0700

    add eco file

--- src/ecosystem.config.ts
+++ src/ecosystem.config.ts
@@ -4,7 +4,9 @@ module.exports = {
       name: "myBackendNestjs",
       script: "dist/src/main.js",
       env: {
-        DATABASE_URL: "postgresql://user:password@host:5432/dbname"
+        DATABASE_URL: "mysql://admin:Cantrinh1995@mydatabase.c5iggco2eked.ap-southeast-2.rds.amazonaws.com:3306/msql",
+        JWT_SECRET: "hskhsierihn3759kfjjruiurkfur",
+        JWT_EXPIRES_IN: "1h"
       }
     }
   ]
