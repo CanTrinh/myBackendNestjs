1deb9f3912e8a7e2a72589a9501a942bb8690160
CanTrinh <cannhai1995@gmail.com>
2026-02-27 16:22:44 +0700

    change to run

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,21 @@ module.exports = {
     {
       name: "myBackendNestjs",
       script: "dist/src/main.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "700M",
       env: {
         DATABASE_URL: "mysql://admin:Saostar1995@mydatabase.c5iggco2eked.ap-southeast-2.rds.amazonaws.com:3306/mysql",
         JWT_SECRET: "hskhsierihn3759kfjjruiurkfur",
         JWT_EXPIRES_IN: "1h"
-      }
+      },
+       post_update: [
+        "npm install",
+        "npm run build",
+        "npx prisma migrate deploy"
+      ]
+
     }
   ]
 };
