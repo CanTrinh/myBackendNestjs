dd3af15cc4e1f5115e1ed8ae9cace7ad0ab53389
CanTrinh <cannhai1995@gmail.com>
2026-02-28 14:10:17 +0700

    chang dbc

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       env_production: {
         DATABASE_URL: "mysql://admin:Cantrinh1995@database-c.c5iggco2eked.ap-southeast-2.rds.amazonaws.com:3306/mysql",
         JWT_SECRET: "hskhsierihn3759kfjjruiurkfur",
-        JWT_EXPIRES_IN: "1h"
+        JWT_EXPIRES_IN: "1h "
       },
        post_update: [
         "npm install",
