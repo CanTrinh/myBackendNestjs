32de75cca06950d721401a0da5d366260a0af095
CanTrinh <cannhai1995@gmail.com>
2026-03-01 09:08:58 +0700

    change evn

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,12 @@ module.exports = {
       env_production: {
         DATABASE_URL: "mysql://admin:Cantrinh1995@database-c.c5iggco2eked.ap-southeast-2.rds.amazonaws.com:3306/can_database",
         JWT_SECRET: "hskhsierihn3759kfjjruiurkfur",
-        JWT_EXPIRES_IN: "1h"
+        JWT_EXPIRES_IN: "1h",
+        // AWS S3 credentials
+        AWS_ACCESS_KEY_ID: "AKIATNLGZDRPUEGJZBND",
+        AWS_SECRET_ACCESS_KEY: "Canteo@95",
+        AWS_REGION: "ap-southeast-2",
+        AWS_S3_BUCKET: "my-frontend-angular"
       },
        post_update: [
         "npm install",
@@ -18,11 +23,6 @@ module.exports = {
         "npx prisma migrate deploy"
       ],
 
-      // AWS S3 credentials
-        AWS_ACCESS_KEY_ID: "AKIATNLGZDRPUEGJZBND",
-        AWS_SECRET_ACCESS_KEY: "Canteo@95",
-        AWS_REGION: "ap-southeast-2",
-        AWS_S3_BUCKET: "my-frontend-angular"
 
 
     }
